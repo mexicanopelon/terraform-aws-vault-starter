eb3b4778d8157bb424a1a0244182e18d7916d9c9
Carlos De La Paz <carlos.paz.c@greenwayhealth.com>
2025-10-29 16:43:48 -0500

    Removed extra outputs.

--- modules/raft_snapshot/outputs.tf
+++ modules/raft_snapshot/outputs.tf
@@ -1,25 +1,10 @@
 # Outputs
-output "s3_bucket_name" {
+output "s3_bucket_vault_snapshots" {
   description = "Name of the S3 bucket for Vault snapshots"
   value       = aws_s3_bucket.vault_snapshots.bucket
 }
 
-output "s3_bucket_arn" {
+output "s3_bucket_vault_snapshots_arn" {
   description = "ARN of the S3 bucket"
   value       = aws_s3_bucket.vault_snapshots.arn
-}
-
-output "iam_role_arn" {
-  description = "ARN of the IAM role for Vault snapshots"
-  value       = aws_iam_role.vault_snapshot_role.arn
-}
-
-output "kms_key_id" {
-  description = "ID of the KMS key for snapshot encryption"
-  value       = aws_kms_key.vault_snapshots.key_id
-}
-
-output "kms_key_arn" {
-  description = "ARN of the KMS key for snapshot encryption"
-  value       = aws_kms_key.vault_snapshots.arn
 }
