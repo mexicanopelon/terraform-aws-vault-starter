5a47011937dbb1673f80e0c5b3cccbddfa477fd6
Carlos De La Paz <carlos.paz.c@greenwayhealth.com>
2025-10-29 20:15:12 -0500

    Removing vault_raft_snapshot_agent_config that needs to be run after Vault cluster is unsealed.

--- modules/raft_snapshot/main.tf
+++ modules/raft_snapshot/main.tf
@@ -42,21 +42,21 @@ resource "aws_s3_bucket_public_access_block" "vault_snapshots" {
 
 
 # Vault Raft Snapshot Agent Configuration
-resource "vault_raft_snapshot_agent_config" "s3_snapshots" {
-  name              = "${var.resource_name_prefix}-s3-snapshot-agent"
-  interval_seconds  = 3600  # Take snapshot every hour
-  retain            = 168   # Retain 168 snapshots (1 week if hourly)
-  path_prefix       = "vault-raft/"
+# resource "vault_raft_snapshot_agent_config" "s3_snapshots" {
+#   name              = "${var.resource_name_prefix}-s3-snapshot-agent"
+#   interval_seconds  = 3600  # Take snapshot every hour
+#   retain            = 168   # Retain 168 snapshots (1 week if hourly)
+#   path_prefix       = "vault-raft/"
   
-  storage_type = "aws-s3"
+#   storage_type = "aws-s3"
   
-  aws_s3_bucket                = aws_s3_bucket.vault_snapshots.bucket
-  aws_s3_region                = var.aws_region
-  aws_s3_enable_kms            = true
-  aws_s3_kms_key               = var.vault_snapshot_kms_key_arn
-  aws_s3_server_side_encryption = true
+#   aws_s3_bucket                = aws_s3_bucket.vault_snapshots.bucket
+#   aws_s3_region                = var.aws_region
+#   aws_s3_enable_kms            = true
+#   aws_s3_kms_key               = var.vault_snapshot_kms_key_arn
+#   aws_s3_server_side_encryption = true
   
-  # If using IAM instance profile, these can be omitted
-  # aws_access_key_id     = var.aws_access_key_id
-  # aws_secret_access_key = var.aws_secret_access_key
-}
+#   # If using IAM instance profile, these can be omitted
+#   # aws_access_key_id     = var.aws_access_key_id
+#   # aws_secret_access_key = var.aws_secret_access_key
+# }
