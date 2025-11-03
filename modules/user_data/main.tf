2ce075a45c2bc82239c05ed6108952d39d1bc763
Carlos De La Paz <carlos.paz.c@greenwayhealth.com>
2025-11-03 14:22:23 -0600

    Fixing region parameter

--- modules/user_data/main.tf
+++ modules/user_data/main.tf
@@ -9,7 +9,7 @@ locals {
   vault_user_data = templatefile(
     var.user_supplied_userdata_path != null ? var.user_supplied_userdata_path : "${path.module}/templates/install_vault.sh.tpl",
     {
-      regional                = var.aws_region
+      region                  = var.aws_region
       name                    = var.resource_name_prefix
       vault_version           = var.vault_version
       kms_key_arn             = var.kms_key_arn
